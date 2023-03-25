8906277da627b1e7040198d8cf583e1253f1e3c4
Eka Starostina <starostinaeka@gmail.com>
2023-03-25 19:35:26 +0300

    New branch TestNG

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -1,9 +1,37 @@
 
 package ru.netology.service;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@Test
+
 public class CashBackHackerServiceTest {
+    @Test
+    public void shouldCalculateMissingQty() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+       Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountEqualBoundary() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountMoreBoundary() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        Assertions.assertEquals(actual, expected);
+    }
 
 }
